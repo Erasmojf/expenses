a82ab382a5a3bb1a6d534362fa6f861ff99d773a
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2023-08-09 18:27:01 -0300

    Create my component expenses App Flutter

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(ExpensesApp());
@@ -10,15 +11,80 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transaction = [
+    Transaction(
+      id: "t1",
+      title: "New Sniker",
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Conta de Luz",
+      value: 250.76,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Pesonal Expenses"),
       ),
-      body: Center(
-        child: Text("Initial Version"),
-      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text("Grafico"),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: _transaction.map((tr) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: const Color.fromARGB(255, 228, 208, 231),
+                          width: 2,
+                        )),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          tr.value.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(tr.title,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          Text(
+                            tr.date.toString(),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ]),
     );
   }
 }

e2c58e07c29bf9294f7aa558f8c5cd68e11ff9a7
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2023-08-09 15:06:18 -0300

    Initial version of my personal expenses App Flutter

---
+++ expenses/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pesonal Expenses"),
+      ),
+      body: Center(
+        child: Text("Initial Version"),
+      ),
+    );
+  }
+}
